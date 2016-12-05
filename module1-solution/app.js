ece9b21f9a065d593b7505efc5e861dacc236447
Daniel Farkas <farkasd@gmail.com>
2016-12-05 16:49:58 +0100

    Handling empty list

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -5,9 +5,13 @@
   .controller('LunchCheckController', function ($scope) {
     $scope.message = "";
     $scope.checkLunch = function () {
-      var lunchLength = $scope.lunchText.split(',').length;
-      if (lunchLength <= 3) $scope.message = "Enjoy!";
-      if (lunchLength > 3) $scope.message = "Too much!";
+      if (!$scope.lunchText || $scope.lunchText.trim() == '') {
+        $scope.message = "Please enter data first"
+      } else {
+        var lunchLength = $scope.lunchText.split(',').length;
+        if (lunchLength <= 3) $scope.message = "Enjoy!";
+        if (lunchLength > 3) $scope.message = "Too much!";
+      }
     }
 
   })
